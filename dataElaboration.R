7f0d44b6349e7969d281c2a04ed96c39e971cb6c
francesco2098 <francesco.diflumeri@studenti.unitn.it>
2021-05-05 11:58:03 +0200

    Read all data

--- dataElaboration.R
+++ dataElaboration.R
@@ -1,4 +1,47 @@
 library(haven)
 
-path = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/", "Cobra baseline EMPTY.sav")
-baseline = read_sav(path)
+pathBaseline = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/", "Cobra baseline EMPTY.sav")
+baseline = read_sav(pathBaseline)
+
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_hads.sav")
+T1Hads = read_sav(pathQoLT1)
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_hads_2.sav")
+T1Hads2 = read_sav(pathQoLT1)
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_contr.sav")
+T1qolContr = read_sav(pathQoLT1)
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_contr_2.sav")
+T1qolContr2 = read_sav(pathQoLT1)
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_pt.sav")
+T1qolPt = read_sav(pathQoLT1)
+pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_pt_2.sav")
+T1qolPt2 = read_sav(pathQoLT1)
+
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_gq_contr.sav")
+T3gqcontr = read_sav(pathQoLT3)
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_gq_pt.sav")
+T3gqPt = read_sav(pathQoLT3)
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_hads.sav")
+T3hads = read_sav(pathQoLT3)
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_qol_contr.sav")
+T3qolContr = read_sav(pathQoLT3)
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_qol_pt.sav")
+T3qolPt = read_sav(pathQoLT3)
+pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_symptoms.sav")
+T3Symptoms = read_sav(pathQoLT3)
+
+pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_gq_contr.sav")
+T4gqContr = read_sav(pathQoLT4)
+pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_gq_pt.sav")
+T4gqPt = read_sav(pathQoLT4)
+pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_hads.sav")
+T4hads = read_sav(pathQoLT4)
+pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_qol_contr.sav")
+T4qolContr = read_sav(pathQoLT4)
+pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_qol_pt.sav")
+T4qolPt = read_sav(pathQoLT4)
+
+
+
+
+
+
