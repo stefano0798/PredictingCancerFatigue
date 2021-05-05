67dfd5371475b44cf74a857f0718205cae075ac7
Stefano Facchini <stefano.facchini@studenti.unitn.it>
2021-05-05 12:47:37 +0200

    Refactor import code and update progress file

--- dataElaboration.R
+++ dataElaboration.R
@@ -1,44 +1,58 @@
 library(haven)
 
-pathBaseline = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/", "Cobra baseline EMPTY.sav")
+data_path = "data/Data"
+T1_path = paste(data_path, "QoLT1", sep = "/")
+T3_path = paste(data_path, "QoLT3", sep = "/")
+T4_path = paste(data_path, "QoLT4", sep = "/")
+
+# read baseline dataset ()
+pathBaseline = file.path(data_path, "Cobra baseline EMPTY.sav")
 baseline = read_sav(pathBaseline)
 
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_hads.sav")
-T1Hads = read_sav(pathQoLT1)
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_hads_2.sav")
-T1Hads2 = read_sav(pathQoLT1)
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_contr.sav")
-T1qolContr = read_sav(pathQoLT1)
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_contr_2.sav")
-T1qolContr2 = read_sav(pathQoLT1)
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_pt.sav")
-T1qolPt = read_sav(pathQoLT1)
-pathQoLT1 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT1", "T1_qol_pt_2.sav")
-T1qolPt2 = read_sav(pathQoLT1)
+# read datasets from T1
+path_T1_hads = file.path(T1_path, "T1_hads.sav")
+path_T1_hads_2 = file.path(T1_path, "T1_hads_2.sav")
+path_T1_qol_contr = file.path(T1_path, "T1_qol_contr.sav")
+path_T1_qol_contr_2 = file.path(T1_path, "T1_qol_contr_2.sav")
+path_T1_qol_pt = file.path(T1_path, "T1_qol_pt.sav")
+path_T1_qol_pt_2 = file.path(T1_path, "T1_qol_pt_2.sav")
 
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_gq_contr.sav")
-T3gqcontr = read_sav(pathQoLT3)
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_gq_pt.sav")
-T3gqPt = read_sav(pathQoLT3)
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_hads.sav")
-T3hads = read_sav(pathQoLT3)
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_qol_contr.sav")
-T3qolContr = read_sav(pathQoLT3)
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_qol_pt.sav")
-T3qolPt = read_sav(pathQoLT3)
-pathQoLT3 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT3", "T3_symptoms.sav")
-T3Symptoms = read_sav(pathQoLT3)
+T1_hads = read_sav(path_T1_hads)
+T1_hads_2 = read_sav(path_T1_hads_2)
+T1_qol_contr = read_sav(path_T1_qol_contr)
+T1_qol_contr_2 = read_sav(path_T1_qol_contr_2)
+T1_qol_pt = read_sav(path_T1_qol_pt)
+T1_qol_pt_2 = read_sav(path_T1_qol_pt_2)
 
-pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_gq_contr.sav")
-T4gqContr = read_sav(pathQoLT4)
-pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_gq_pt.sav")
-T4gqPt = read_sav(pathQoLT4)
-pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_hads.sav")
-T4hads = read_sav(pathQoLT4)
-pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_qol_contr.sav")
-T4qolContr = read_sav(pathQoLT4)
-pathQoLT4 = file.path("C:/", "Users/utente/Desktop/Drawer/EIT_MasterSchool/REDI/pcf_project/data/Data/QoLT4", "T4_qol_pt.sav")
-T4qolPt = read_sav(pathQoLT4)
+
+# read datasets from T3
+path_T3_gq_contr = file.path(T3_path, "T3_gq_contr.sav")
+path_T3_gq_pt = file.path(T3_path, "T3_gq_pt.sav")
+path_T3_hads = file.path(T3_path, "T3_hads.sav")
+path_T3_qol_contr = file.path(T3_path, "T3_qol_contr.sav")
+path_T3_qol_pt = file.path(T3_path, "T3_qol_pt.sav")
+path_T3_symptoms = file.path(T3_path, "T3_symptoms.sav")
+
+T3_gq_contr = read_sav(path_T3_gq_contr)
+T3_gq_pt = read_sav(path_T3_gq_pt)
+T3_hads = read_sav(path_T3_hads)
+T3_qol_contr = read_sav(path_T3_qol_contr)
+T3_qol_pt = read_sav(path_T3_qol_pt)
+T3_symptoms = read_sav(path_T3_symptoms)
+
+
+# read datasets from T4
+path_T4_gq_contr = file.path(T4_path, "T4_gq_contr.sav")
+path_T4_gq_pt = file.path(T4_path, "T4_gq_pt.sav")
+path_T4_hads = file.path(T4_path, "T4_hads.sav")
+path_T4_qol_contr = file.path(T4_path, "T4_qol_contr.sav")
+path_T4_qol_pt = file.path(T4_path, "T4_qol_pt.sav")
+
+T4_gq_contr = read_sav(path_T4_gq_contr)
+T4_gq_pt = read_sav(path_T4_gq_pt)
+T4_hads = read_sav(path_T4_hads)
+T4_qol_contr = read_sav(path_T4_qol_contr)
+T4_qol_pt = read_sav(path_T4_qol_pt)
 
 
 
